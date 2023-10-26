5d4878fb6d8e008e42afdf046f553cfe6581b0db
Shivani <developerweb6@gmail.com>
2023-10-26 15:05:53 +0530

    modified

--- src/components/SuperAdminDashboard/Trips/PendingTrips.jsx
+++ src/components/SuperAdminDashboard/Trips/PendingTrips.jsx
@@ -194,15 +194,15 @@ const SuperPendingTrip = () => {
               <div className="body flex-grow-1 px-3">
                 <h1 className="heading-for-every-page">Pending Trips</h1>
                 <div className="active-trip-outer">
-                  {/* <div className="trips-head d-flex justify-content-between">
+                   <div className="trips-head d-flex justify-content-between">
                     <div className="box-shd d-flex justify-content-between">
                       <div className="left-trip-content">
                         <h2>List of Pending Trips</h2>
                       </div>
                       <div className="right-trip-content">
-                        <img src={refreshImg} />
+                        {/* <img src={refreshImg} />
                         <img src={downarrowImg} />
-                        <img src={crossImg} />
+                        <img src={crossImg} /> */}
                         <Link to="/superadmindashboard/trips/addnewbooking">
                           <CButton className="add_company_btn">Add New Booking</CButton>
                         </Link>
