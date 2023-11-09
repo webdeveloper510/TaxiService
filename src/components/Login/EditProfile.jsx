0fdbb19827d897e5aecb455283c022694f7b7cd1
Shivani <developerweb6@gmail.com>
2023-11-09 17:39:33 +0530

    add change password to customer edit

--- src/components/Login/EditProfile.jsx
+++ src/components/Login/EditProfile.jsx
@@ -210,7 +210,7 @@ function EditProfile() {
                                   name="Address1"
                                   autoComplete="off" />   
                               </CCol>
-                              <CCol xs={6}>
+                              <CCol md={6}>
                               <CButton className="change_pwd_btn" onClick={() => setVisible(!visible)}>Change Password</CButton>
                               </CCol>
                        
@@ -236,7 +236,7 @@ function EditProfile() {
       onClose={() => setVisible(false)}
     >
       <CModalHeader onClose={() => setVisible(false)}>
-        <CModalTitle id="changepassword">Change Passowrd</CModalTitle>
+        <CModalTitle id="changepassword">Change Password</CModalTitle>
       </CModalHeader>
       <CModalBody>
                       <CRow>
@@ -284,7 +284,7 @@ function EditProfile() {
 
                 <div className="mb-4" id="pwd_field">
                   <label htmlFor="password" className="form-label">
-                  Nex  Password
+                  New  Password
                   </label>
                   <MDBInput
                     id="password"
