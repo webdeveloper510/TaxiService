fa5ce9c441367b68b6756cb5ddb69bf6b8eeb17c
RAVI SHARMA <136293401+ravicodenomad@users.noreply.github.com>
2023-11-23 17:47:51 +0530

    all bugs fixes

--- src/components/Driver/PastTrips.jsx
+++ src/components/Driver/PastTrips.jsx
@@ -85,9 +85,16 @@ const PastTrips = ({type}) => {
           to={`/trips/view-trip-details/${params.row._id}`}
         >
           <button style={{
-            width:"80px",
+            width:"40px",
             height:"40px",
-          }} className="submit-btn">Show</button>
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#fff2cf"
+          }} className="submit-btn"><svg xmlns="http://www.w3.org/2000/svg" width="90px" height="90px" fill="black" class="bi bi-info-circle" viewBox="0 0 16 16" strokeWidth="8">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+        </svg></button>
         </Link>
       ),
     },
@@ -144,7 +151,7 @@ const PastTrips = ({type}) => {
     },
     {
       field: "price",
-      headerName: "Earning",
+      headerName: "Earning in Euro",
       width: 200,
      
     },
@@ -152,7 +159,7 @@ const PastTrips = ({type}) => {
       field: "commission",
       headerName: "Commission",
       width: 200,
-      valueGetter: (params) => params.row.price
+      valueGetter: (params) =>"10%"
      
     },
     
@@ -163,10 +170,17 @@ const PastTrips = ({type}) => {
       renderCell: (params) => (
         
           <button style={{
-            width:"1200px",
+            width:"40px",
             height:"40px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#fff2cf"
           }}
-          onClick={()=>downloadPdf(params.row,user)} className="submit-btn">Download</button>
+          onClick={()=>downloadPdf(params.row,user)} className="submit-btn"><svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+          <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+          <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+        </svg></button>
         
       ),
     },
--- src/utils/pdfHtml.js
+++ src/utils/pdfHtml.js
@@ -1,6 +1,64 @@
 export const generateInvoiceHtml = (data) => {
-   
   
-    return `<div id="mainPdf" style="display:flex; justify-content:center;height:1000px; width: 100%; margin:auto;"><div style=" margin: 0 auto; padding: 30px; border: 3px solid #ddd; height:700px;width: 95%"><div ><h1 style="text-align: center; color: #333;">Invoice</h1><p style="letter-spacing: 2px;font-size:22px"><strong>Driver Name:</strong> <span style="color: #333;letter-spacing: 2px;">${data.driverName}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Customer Name:</strong> <span style="color: #333;letter-spacing: 2px;">${data.customerName}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Driver Phone Number:</strong> <span style="color: #333;letter-spacing: 2px;">${data.driverPhone}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Trip ID:</strong> <span style="color: #333;letter-spacing: 2px;">${data._id}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Pickup Date:</strong> <span style="color: #333;letter-spacing: 2px;">${data.pickupDate}</span></p></div><table style="width: 100%; border-collapse: collapse; margin-top: 20px;"><thead><tr><th style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">Trip From</th><th style="border: 2px solid #ddd; padding: 8px;font-size:22px;letter-spacing: 2px;">Trip To</th><th style="border: 2px solid #ddd; padding: 8px;font-size:22px;letter-spacing: 2px;">Price</th></tr></thead><tbody id="items"><tr><td style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">${data.tripFrom}</td><td style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">${data.tripTo}</td><td style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">${data.price}</td></tr></tbody></table><p style="letter-spacing: 2px;font-size:22px; margin-top:5px;"><strong>Vehicle Type:</strong> <span style="color: #333;letter-spacing: 2px;font-size:22px">${data.vehicleTrip}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Payment Type:</strong> <span style="color: #333;letter-spacing: 2px;font-size:22px">${data.payOption}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Net Total:</strong> <span style="color: #333;letter-spacing: 2px;font-size:22px">${data.price}</span></p></div></div>`;
+    return `<div id="mainPdf" style="height: 1000px; width: 100%; display: flex; font-family: sans-serif; padding-left:40px; justify-content: center;">
+    <div style="margin: auto; width:98%; padding: 10px 30px; border: 3px solid #ddd; height: 700px;">
+        <div style="text-align: left;">
+            <h1 style="text-align: center; color: #000; font-weight:600;">Invoice</h1>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Driver Name:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px;  font-size: 22px;">${data.driverName}</span>
+            </p>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Customer Name:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px;  font-size: 22px;">${data.customerName}</span>
+            </p>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Driver Phone Number:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px;  font-size: 22px;">${data.driverPhone}</span>
+            </p>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Trip ID:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px; font-size: 22px;">${data._id}</span>
+            </p>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Pickup Date:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px; font-size: 22px;">${data.pickupDate}</span>
+            </p>
+        </div>
+        <table style="width: 100%; border-collapse: collapse; margin-top: 40px;">
+            <thead>
+                <tr>
+                    <th style="border: 2px solid #6c6969; background: #dbe0e3de; padding: 8px; font-size: 22px; letter-spacing: 2px;">Trip From</th>
+                    <th style="border: 2px solid #6c6969; background: #dbe0e3de; padding: 8px; font-size: 22px; letter-spacing: 2px;">Trip To</th>
+                    <th style="border: 2px solid #6c6969; background: #dbe0e3de; padding: 8px; font-size: 22px; letter-spacing: 2px;">Price</th>
+                </tr>
+            </thead>
+            <tbody id="items">
+                <tr>
+                    <td style="border: 2px solid #6c6969; padding: 8px; font-size: 22px; letter-spacing: 2px;">${data.tripFrom}</td>
+                    <td style="border: 2px solid #6c6969; padding: 8px; font-size: 22px; letter-spacing: 2px;">${data.tripTo}</td>
+                    <td style="border: 2px solid #6c6969; padding: 8px; font-size: 22px; letter-spacing: 2px;">${data.price}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div style="text-align: left;">
+            <p style="letter-spacing: 2px; font-size: 22px; margin-top: 30px;">
+                <strong>Vehicle Type:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px; font-size: 22px;">${data.vehicleTrip}</span>
+            </p>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Payment Type:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px; font-size: 22px;">${data.payOption}</span>
+            </p>
+            <p style="letter-spacing: 2px; font-size: 22px;">
+                <strong>Net Total:</strong>
+                <span style="color: #4a4a4c; letter-spacing: 2px; margin-left: 10px; font-size: 22px;">${data.price}</span>
+            </p>
+        </div>
+    </div>
+</div>`;
+  
+    // return `<div id="mainPdf" style="height:1000px; width: 100%; display:flex;
+    //  padding: 10px; justify-content:center;"><div style=" margin: auto; padding: 30px; border: 3px solid #ddd; height:700px; width: 99%;"><div ><h1 style="text-align: center; color: #333;">Invoice</h1><p style="letter-spacing: 2px;font-size:22px"><strong>Driver Name:</strong> <span style="color: #333;letter-spacing: 2px;">${data.driverName}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Customer Name:</strong> <span style="color: #333;letter-spacing: 2px;">${data.customerName}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Driver Phone Number:</strong> <span style="color: #333;letter-spacing: 2px;">${data.driverPhone}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Trip ID:</strong> <span style="color: #333;letter-spacing: 2px;">${data._id}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Pickup Date:</strong> <span style="color: #333;letter-spacing: 2px;">${data.pickupDate}</span></p></div><table style="width: 100%; border-collapse: collapse; margin-top: 20px;"><thead><tr><th style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">Trip From</th><th style="border: 2px solid #ddd; padding: 8px;font-size:22px;letter-spacing: 2px;">Trip To</th><th style="border: 2px solid #ddd; padding: 8px;font-size:22px;letter-spacing: 2px;">Price</th></tr></thead><tbody id="items"><tr><td style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">${data.tripFrom}</td><td style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">${data.tripTo}</td><td style="border: 2px solid #ddd; padding: 8px; font-size:22px;letter-spacing: 2px;">${data.price}</td></tr></tbody></table><p style="letter-spacing: 2px;font-size:22px; margin-top:5px;"><strong>Vehicle Type:</strong> <span style="color: #333;letter-spacing: 2px;font-size:22px">${data.vehicleTrip}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Payment Type:</strong> <span style="color: #333;letter-spacing: 2px;font-size:22px">${data.payOption}</span></p><p style="letter-spacing: 2px;font-size:22px"><strong>Net Total:</strong> <span style="color: #333;letter-spacing: 2px;font-size:22px">${data.price}</span></p></div></div>`;
   };
   
