6e8c0509c3c6c5a2dca6192256de08407b6a4e26
RAVI SHARMA <136293401+ravicodenomad@users.noreply.github.com>
2023-12-16 16:24:20 +0530

    success cancel

--- src/components/Driver/Cancel.jsx
+++ src/components/Driver/Cancel.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router';
 import SidebarDriver from './Sidebar';
 import AppHeader from '../TopBar/AppHeader';
 import { failedTripCommission } from '../../utils/api';
+import { CCol, CRow } from '@coreui/react';
 
 
 const CancelPaymentPage = () => {
@@ -36,14 +37,28 @@ const CancelPaymentPage = () => {
               style={{ paddingBottom: "20px" }}
             >
               <h1 class="heading-for-every-page">Trip</h1>
-
+             
               {/* <div class="active-trip-outer"> */}
                {loading? <AppLoader/>: 
-              <div>
-              <h1>Payment Canceled!</h1>
-              <p>Please payment again</p>
-            </div>
-                }
+               <CRow>
+               <CCol xs={2}></CCol>
+               <CCol xs={8}>
+                 <div style={{
+                   height:"50vh"
+                 }} class="active-trip-outer text-center d-flex justify-content-center align-items-center mx-5 p-4">
+                 <div>
+                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" 
+                 style={{color:"red", marginBottom:"20px"}} class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+</svg>
+<h1>Payment Canceled!</h1>
+           <p>Please payment again</p>
+                 </div>
+                 </div>
+               </CCol>
+               <CCol xs={2}></CCol>
+             </CRow>
+}
             </div>
           </div>
         </div>
--- src/components/Driver/Success.jsx
+++ src/components/Driver/Success.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router';
 import SidebarDriver from './Sidebar';
 import AppHeader from '../TopBar/AppHeader';
 import { successTripCommission } from '../../utils/api';
+import { CCol, CRow } from '@coreui/react';
 
 
 const SuccessPaymentPage = () => {
@@ -36,13 +37,31 @@ const SuccessPaymentPage = () => {
             >
               <h1 class="heading-for-every-page">Trip Payment Done</h1>
 
+             
               {/* <div class="active-trip-outer"> */}
                {loading? <AppLoader/>: 
               <div>
-              <h1>Payment Successful!</h1>
+              <CRow>
+                  <CCol xs={2}></CCol>
+                  <CCol xs={8}>
+                    <div style={{
+                      height:"50vh"
+                    }} class="active-trip-outer text-center d-flex justify-content-center align-items-center mx-5 p-4">
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" 
+                    style={{color:"green", marginBottom:"20px"}}
+                    class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg>
+                    <h1>Payment Successful!</h1>
               <p>Thank you for your payment.</p>
+                    </div>
+                    </div>
+                  </CCol>
+                  <CCol xs={2}></CCol>
+                </CRow>
             </div>
-                }
+}
             </div>
           </div>
         </div>
