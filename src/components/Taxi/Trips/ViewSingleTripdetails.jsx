df7c519d8378ee19948395be47a4ffc6283abd9e
designerCodenomad <designer@codenomad.net>
2024-01-08 17:07:35 +0530

    single page bug fix
    Signed-off-by: designerCodenomad <designer@codenomad.net>

--- src/components/Taxi/Trips/ViewSingleTripdetails.jsx
+++ src/components/Taxi/Trips/ViewSingleTripdetails.jsx
@@ -36,58 +36,60 @@ import SuperMap from "../../TaxiMap/Map";
 import userContext from "../../../utils/context";
 import SuperAdminSideBar from "../../SuperAdmin/Sidebar/SideBar";
 import SidebarDriver from "../../Driver/Sidebar";
+import SideBar2 from "../../Hotel/SideBar2";
 const ViewSingleTrip = () => {
   const [trip, setTrip] = useState(null)
   const [customerName, setCustomerName] = useState(null);
   const [loading, setLoading] = useState(false);
-  const {vehicleId} = useParams();
-  const {user,setUser,appLoaded} = useContext(userContext);
- const navigate = useNavigate();
- const id = useParams().id;
- useEffect(()=>{
-  if(appLoaded){
-    if(!user){
-      navigate("/")
-    }
-    setLoading(true);
-    getTripById(id).then((res)=>{
-      console.log("page data for trip",res);
-      if(res.code === 200){
-        setTrip(res.result)
-        if(res.hotelName){setCustomerName(res.hotelName)}
+  const { vehicleId } = useParams();
+  const { user, setUser, appLoaded } = useContext(userContext);
+  const navigate = useNavigate();
+  const id = useParams().id;
+  useEffect(() => {
+    if (appLoaded) {
+      if (!user) {
+        navigate("/")
       }
-    }).catch((err)=>{
-      console.log(err);
-    }).finally(()=>{
-      setLoading(false);
-    })
-  }
- },[appLoaded])
+      setLoading(true);
+      getTripById(id).then((res) => {
+        console.log("page data for trip", res);
+        if (res.code === 200) {
+          setTrip(res.result)
+          if (res.hotelName) { setCustomerName(res.hotelName) }
+        }
+      }).catch((err) => {
+        console.log(err);
+      }).finally(() => {
+        setLoading(false);
+      })
+    }
+  }, [appLoaded])
 
- 
+
   return (
     <>
       <div className="container-fluidd">
 
         <div className="col-md-12">
           <div>
-             {user?.role === "SUPER_ADMIN" && <SuperAdminSideBar/>} 
-              {user?.role === "COMPANY" && <SuperSideBar/>}
-              {user?.role === "DRIVER" && <SidebarDriver/>}
-              
+            {user?.role === "SUPER_ADMIN" && <SuperAdminSideBar />}
+            {user?.role === "COMPANY" && <SuperSideBar />}
+            {user?.role === "DRIVER" && <SidebarDriver />}
+            {user?.role === "HOTEL" && <SideBar2 />}
+
             <div className="wrapper d-flex flex-column min-vh-100 bg-light">
               <AppHeader />
-             {loading ? <AppLoader/> : <div className="body flex-grow-1 px-3" style={{ paddingBottom: "20px" }}>
-               <h1 class="heading-for-every-page">
-               <Link to="/" className="view_detail">
+              {loading ? <AppLoader /> : <div className="body flex-grow-1 px-3" style={{ paddingBottom: "20px" }}>
+                <h1 class="heading-for-every-page">
+                  <Link to="/" className="view_detail">
                     <img src={backtovehicle} alt="edit" /> View Trip Details
-                    </Link>
-                    </h1>
-                   
+                  </Link>
+                </h1>
+
                 <div class="active-trip-outer">
                   {/* <h2>Add New Vehicle</h2> */}
                   {/********** vehicle---information---form *****************/}
-                 <CRow>
+                  <CRow>
 
                     <CCol xs={12}>
                       <CCard className="mb-4">
@@ -95,58 +97,58 @@ const ViewSingleTrip = () => {
                           <strong>Vehicle Information</strong>
                         </CCardHeader> */}
                         <CCardBody>
-                        
+
                           <form noValidate className="row g-3 view_single_trip_form">
-                         
-                          <CCol md={5} className="single-trip-map">
-                            
-                          <SuperMap/>
-                          {/* <img className="vehicle_img" src={} alt="Car photo"/> */}
-                           </CCol>
-                           
-                            <CCol className="vehicle_info_right" md={7}>
-                            <div class="trip-details">
-                              <CFormLabel htmlFor="inputvehiclenum"><img src={checkedImg}/>Trip ID :</CFormLabel>
-                              <span className="vehicle_info">{trip?.trip_id}</span> <br/></div>
-                              <div class="trip-details">
-                              {customerName && <CFormLabel htmlFor="inputvehiclenum"><img src={checkedImg}/>Customer Name :</CFormLabel>}
-                              <span className="vehicle_info">{customerName} </span><br/></div>
-                              <div class="trip-details">
-                                  <CFormLabel htmlFor="inputvehivlemodal"><img src={checkedImg}/>Trip From :</CFormLabel>
-                             
-                                 <span className="vehicle_info"> {trip?.trip_from?.address}</span><br/></div>
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputseating"><img src={checkedImg}/>Trip To :</CFormLabel>
-                                 <span className="vehicle_info"> {trip?.trip_to?.address}</span><br/></div>
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassenger"><img src={checkedImg}/>Date :</CFormLabel>
-                                 <span className="vehicle_info">{moment(trip?.pickup_date_time).format("MMM Do YYYY")}</span><br/></div>
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassengercharges"><img src={checkedImg}/>Time :</CFormLabel>
-                                 <span className="vehicle_info">{moment(trip?.pickup_date_time).format("h:mm a")}</span><br/></div>
-                                 {trip?.driver_name && <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassengercharges"><img src={checkedImg}/>Drive Name :</CFormLabel>
-                                 <span className="vehicle_info">{trip?.driver_name}</span><br/></div>}
-                                 {(trip?.price!= null) && <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassengercharges"><img src={checkedImg}/> Fare (in €) :</CFormLabel>
-                                 <span className="vehicle_info">{trip?.price}</span><br/></div>}
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassenger"><img src={checkedImg}/>Commission Type :</CFormLabel>
-                                 <span className="vehicle_info">{trip?.commission?.commission_type}</span><br/></div>
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassenger"><img src={checkedImg}/>Commission Value :</CFormLabel>
-                                 <span className="vehicle_info">{trip?.commission?.commission_value}</span><br/></div>
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassenger"><img src={checkedImg}/>Email :</CFormLabel>
-                                 <span className="vehicle_info">{trip?.email}</span><br/></div>
-                                 <div class="trip-details">
-                                 <CFormLabel htmlFor="inputpassenger"><img src={checkedImg}/>Phone :</CFormLabel>
-                                 <span className="vehicle_info">{trip?.phone}</span><br/></div>
-                                 
+
+                            <CCol md={5} className="single-trip-map">
+
+                              <SuperMap />
+                              {/* <img className="vehicle_img" src={} alt="Car photo"/> */}
                             </CCol>
 
-                      
-       
+                            <CCol className="vehicle_info_right" md={7}>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputvehiclenum"><img src={checkedImg} />Trip ID :</CFormLabel>
+                                <span className="vehicle_info">{trip?.trip_id}</span> <br /></div>
+                              <div class="trip-details">
+                                {customerName && <CFormLabel htmlFor="inputvehiclenum"><img src={checkedImg} />Customer Name :</CFormLabel>}
+                                <span className="vehicle_info">{customerName} </span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputvehivlemodal"><img src={checkedImg} />Trip From :</CFormLabel>
+
+                                <span className="vehicle_info"> {trip?.trip_from?.address}</span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputseating"><img src={checkedImg} />Trip To :</CFormLabel>
+                                <span className="vehicle_info"> {trip?.trip_to?.address}</span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassenger"><img src={checkedImg} />Date :</CFormLabel>
+                                <span className="vehicle_info">{moment(trip?.pickup_date_time).format("MMM Do YYYY")}</span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassengercharges"><img src={checkedImg} />Time :</CFormLabel>
+                                <span className="vehicle_info">{moment(trip?.pickup_date_time).format("h:mm a")}</span><br /></div>
+                              {trip?.driver_name && <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassengercharges"><img src={checkedImg} />Drive Name :</CFormLabel>
+                                <span className="vehicle_info">{trip?.driver_name}</span><br /></div>}
+                              {(trip?.price != null) && <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassengercharges"><img src={checkedImg} /> Fare (in €) :</CFormLabel>
+                                <span className="vehicle_info">{trip?.price}</span><br /></div>}
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassenger"><img src={checkedImg} />Commission Type :</CFormLabel>
+                                <span className="vehicle_info">{trip?.commission?.commission_type}</span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassenger"><img src={checkedImg} />Commission Value :</CFormLabel>
+                                <span className="vehicle_info">{trip?.commission?.commission_value}</span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassenger"><img src={checkedImg} />Email :</CFormLabel>
+                                <span className="vehicle_info">{trip?.email}</span><br /></div>
+                              <div class="trip-details">
+                                <CFormLabel htmlFor="inputpassenger"><img src={checkedImg} />Phone :</CFormLabel>
+                                <span className="vehicle_info">{trip?.phone}</span><br /></div>
+
+                            </CCol>
+
+
+
 
 
                           </form>
