e5522bf319ac83ca49263be0c020c114c46ea8f3
RAVI SHARMA <136293401+ravicodenomad@users.noreply.github.com>
2024-02-29 11:57:09 +0530

    done

--- src/components/Taxi/Trips/allocatemap.jsx
+++ src/components/Taxi/Trips/allocatemap.jsx
@@ -111,6 +111,14 @@ const Allocatemap=()=> {
     rotation: 0,
     scale: 1,
   };
+  const customMarker2 = {
+    path: "M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759 c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713 v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336 h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805",
+    fillColor: "yellow",
+    fillOpacity: 2,
+    strokeWeight: 1,
+    rotation: 0,
+    scale: 1,
+  };
   const generateIcon = (driver)=>{
     return {
         path: "M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759   c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z    M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713   v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336   h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805",
@@ -123,7 +131,7 @@ const Allocatemap=()=> {
   }
   const [selectedMarker, setSelectedMarker] = useState(null);
   const handleMarkerClick = (marker) => {
-    if(marker.is_available)    setSelectedMarker(marker);
+    if(marker.is_available) setSelectedMarker(marker);
   };
 
   const handleALLocate = () => {
@@ -186,18 +194,19 @@ const Allocatemap=()=> {
        
           zoom={10}
           onClick={handleMapClick}
-
+          options={{
+            streetViewControl: false
+          }}
         > 
         {
           driverLocation.map(driver=>(
-            <Marker 
+            <Marker
             position={{ lat: driver.lat, lng: driver.lng }}
             icon={generateIcon(driver)}
             onClick={() => handleMarkerClick(driver)}
-          
             label={{
                 text: driver.first_name + ' ' + driver.last_name,
-                color: 'black',  fontSize: '18px', fontWeight: 'bold',background: '#3498db !important', borderRadius: '5px !important',boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1) !important',padding: '8px !important', }}
+                color: 'black',  fontSize: '18px', fontWeight: 'bold',backgroundColor: '#3498db !important', borderRadius: '5px !important',boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1) !important',padding: '8px !important', }}
           />
           ))
         }
@@ -211,11 +220,11 @@ const Allocatemap=()=> {
                         
                         <div className="text-center">
                         <h3 className="vichle-name">Driver Details</h3>
-                        <img src={carLogo} alt="car logo" width="100px" height="100px" />
+                        <img src={selectedMarker?.defaultVehicle?.vehicle_photo} alt="car logo" width="100px" height="100px" />
                         </div>
                         <h5 className="driver-name">Driver Name : {selectedMarker.first_name + ' ' + selectedMarker.last_name}</h5>
-                        <h4 className="vichle-name">Vehicle Name : Bike</h4>
-                        <h4 className="vichle-name">Vichke Type : test</h4>
+                        <h4 className="vichle-name">Vehicle Name :{`${selectedMarker?.defaultVehicle?.vehicle_make} ${selectedMarker.defaultVehicle.vehicle_model}`}</h4>
+                        <h4 className="vichle-name">Vichke Type :{selectedMarker?.defaultVehicle?.vehicle_type}</h4>
                        <div class="text-center">
                        <button className="approve-butn" onClick={handleALLocate}>Approve</button>
                        </div>
